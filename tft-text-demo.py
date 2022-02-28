0cf58022218fe1f347364a1fd40ff512f8ca37b3
Ale Campos <acc726@nyu.edu>
2022-02-28 05:05:01 -0500

    Added print text

--- tft-text-demo.py
+++ tft-text-demo.py
@@ -1,12 +1,9 @@
-#Ale Campos
-#Demo file:
-#prints an image of a snake onto the tft 1.8" lcd
-
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 """
-Be sure to check the learn guides for more usage information.
+This demo will draw a few rectangles onto the screen along with some text
+on top of that.
 
 This example is for use on (Linux) computers that are using CPython with
 Adafruit Blinka to support CircuitPython libraries. CircuitPython does
@@ -17,14 +14,12 @@ Author(s): Melissa LeBlanc-Williams for Adafruit Industries
 
 import digitalio
 import board
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import st7735  # pylint: disable=unused-import
 
-
-# Turn on the Backlight
-backlight = digitalio.DigitalInOut(board.D25)
-backlight.switch_to_output()
-backlight.value = True
+# First define some constants to allow easy resizing of shapes.
+BORDER = 32
+FONTSIZE = 18
 
 # Configuration for CS and DC pins (these are PiTFT defaults):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -55,32 +50,31 @@ if disp.rotation % 180 == 90:
 else:
     width = disp.width  # we swap height/width to rotate it to landscape!
     height = disp.height
+
 image = Image.new("RGB", (width, height))
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+# Draw a BLACK filled box as the background
+draw.rectangle((0, 0, width, height), fill=(0, 0, 0))
 disp.image(image)
 
-image = Image.open("McLaren.jpg")
+# Draw a smaller inner BLUE rectangle
+#draw.rectangle((BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(255, 0, 0))
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+# Load a TTF Font
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
+# Draw Some Text
+text = input("Please enter text: ")
+(font_width, font_height) = font.getsize(text)
+draw.text(
+    (width // 2 - font_width // 2, height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=(255, 255, 0),
+)
 
 # Display image.
 disp.image(image)
