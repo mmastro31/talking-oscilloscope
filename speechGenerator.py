9e05b1aa5cc9160c2d5a9a13dd1635f2479fbbf2
mmastro31 <55255682+mmastro31@users.noreply.github.com>
2022-04-20 20:54:06 -0400

    Update speechGenerator.py

--- speechGenerator.py
+++ speechGenerator.py
@@ -3,13 +3,14 @@ import time
 
 scope = oscilloscope.Oscilloscope()
 scope.setupSound()
-
+'''
 voices = scope.engine.getProperty('voices')
 for voice in voices:
    print(voice, voice.id)
    scope.engine.setProperty('voice', voice.id)
    scope.engine.say('The quick brown fox jumped over the lazy dog.')
 scope.engine.runAndWait()
+'''
 
 lines = [
     'Single Shot Voltage',
@@ -29,6 +30,9 @@ lines = [
     'You are now ready to begin measuring. Press play when you are ready.',
     'Welcome to Advanced Mode. Please select a measurement',
 ]
+
+scope.engine.setProperty('voice', 'english_rp+f3')
+scope.engine.say('The quick brown fox jumped over the lazy dog.')
 
 '''
 for i in range(len(lines)):
